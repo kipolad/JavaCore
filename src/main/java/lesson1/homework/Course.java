e3d56e05bb3a64fb1b3b57f972886970b95639a0
telysheva <kipolad@bk.ru>
2022-03-22 22:50:42 +1100

    Class Course was edited

--- src/main/java/lesson1/homework/Course.java
+++ src/main/java/lesson1/homework/Course.java
@@ -17,13 +17,23 @@ public class Course {
     }
 
     void doIt(Team team) {
+        for (int i = 5; i >= 0; i--) {
+            System.out.println(i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Начали!");
         for (Participant participant : team.getParticipants()) {
             for (Trial trial : trials) {
                 if (participant.getMaxRunningDistance() > trial.getDistance()) {
                     participant.setResult(participant.getResult() + 1);
                 }
             }
-
         }
+        System.out.println("Соревнования прошли успешно!");
     }
 }
